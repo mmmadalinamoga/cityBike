6b9e7932a75e0db979e6b47f06e22cce115dfe51
Madalina <madalina.m.moga@gmail.com>
2020-05-10 18:32:10 +0300

    data in

--- adapter/src/adapter.py
+++ adapter/src/adapter.py
@@ -6,60 +6,72 @@ import paho.mqtt.client as mqtt
 import json
 import time
 
-client_db = None
-client_mqtt = None
 
 # Upon connecting to the mqtt broker, subscribe to all 
 # its topics
 def on_connect(client_mqtt, userdata, flags, rc):
-	print("Connected with result code: " + str(rc))
-	client_mqtt.subscribe("#")
+    print("Connected with result code: " + str(rc))
+    client_mqtt.subscribe("station_data")
 
 # Upon receiving a message, write it to the
 # influx database
 def on_message(client_mqtt, userdata, msg):
     msg_decoded = str(msg.payload.decode())
     print("Message received", msg_decoded)
-	
-    #TODO: CHECK STATION AUTHENTICITY
+    
+    # Unpack received data in a dictionary:
+    received_dict = json.loads(msg_decoded)
+    print("Dict received", received_dict)
 
-	# Unpack received data in a dictionary:
-    received_dict = json.loads(m_decode)
+    # Add local timestamp if no timestamp was found    
+    if "timestamp" in received_dict.keys():
+        timestamp = received_dict["timestamp"]
+    else: 
+        t = time.localtime()
+        timestamp = time.strftime("%H:%M:%S", t)
 
-	# Add local timestamp if no timestamp was found	
-	if "timestamp" in received_dict.keys() :
-		timestamp = received_dict["timestamp"]
-	else : 
-		t = time.localtime()
-		timestamp = time.strftime("%H:%M:%S", t)
-	
-	#TODO: Only write numeric data to database
-	# for station in received_dict.keys() :
+    message_body = [{"measurement":"station_data",
+    				 "tags" : { "bike_key_dispenser": received_dict["dispenser"],
+                				"bike_keys_available": received_dict["keys"],
+    				 			"id" : received_dict["id"],
+    				 			"name" : received_dict["name"],
+    				 			"battery" : received_dict["battery"],
+    				},
+    				"timestamp" : timestamp,
+    				"fields" : {
+    							"available_bikes": received_dict["bikes"],
+                				"available_docks": received_dict["docks"],
+                				"unavailable_bikes": received_dict["ubikes"],
+                				"unavailable_docks": received_dict["udocks"]
+    				}
+    				}]
+
+    client_db.write_points(message_body);
+    #result = client_db.query("SELECT available_bikes from station_data;")
+    result = client_db.query(query='select available_bikes from station_data where "name"=$name;', bind_params={"name": 'unirii'})
+    print("Result: {0}".format(result))
 
 
 # Write received data to stdout
 def on_log(client_mqtt, userdata, level, buf):
-	print("log: ", buf)
+    print("Adapter log: ", buf)
 
  
 if __name__ == '__main__':
-	global client_mqtt
-	global client_db
-	
-	# Instantiate a connection to the InfluxDB
-	client_db = InfluxDBClient('influxdb', '8086', '', '', 'citybikedb')
-	
-	# Create an mqtt client and connect to broker
-	client_mqtt = mqtt.Client("citybike")
-	client_mqtt.connect('mosquitto', 1883)
-	
-	# Set callbacks for this client
-	client_mqtt.on_connect = on_connect
-	client_mqtt.on_message = on_message
-	client_mqtt.on_log = on_log
+    # Instantiate a connection to the InfluxDB
+    client_db = InfluxDBClient(host='influxdb', port='8086', username='', password='', database='iotdb')
+    
+    # Create an mqtt client and connect to broker
+    client_mqtt = mqtt.Client("adapter")
+    client_mqtt.connect('mosquitto', 1883)
+    
+    # Set callbacks for this client
+    client_mqtt.on_connect = on_connect
+    client_mqtt.on_message = on_message
+    client_mqtt.on_log = on_log
 
-	client_mqtt.loop_forever()
+    client_mqtt.loop_forever()
 
-	# Drop database
-	client_db.drop_database(dbname)
+    # Drop database
+    client_db.drop_database(dbname)
 
